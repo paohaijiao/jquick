fc40d056a490d8f86bab070c0633b4beebb789f3
martin <goudingcheng@gmail.com>
2025-06-14 07:38:50 +0800

    bugfix: generate relation chart data

---
+++ jquick-pdf/jquick-pdf-svg/src/main/java/com/paohaijiao/echart/generate/JRelationChartRenderer.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.paohaijiao.echart.generate;
+
+import com.paohaijiao.data.JOption;
+import com.paohaijiao.data.series.JGraph;
+import com.paohaijiao.data.series.force.JLink;
+import org.apache.batik.anim.dom.SVGDOMImplementation;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.*;
+import java.util.List;
+
+/**
+ * packageName com.paohaijiao.echart.generate
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @className JHeatMap
+ * @date 2025/6/13
+ * @description
+ */
+public class JRelationChartRenderer {
+    public static void generateHeatmapSVG( List<Map<String, Object>> nodes,List<Map<String, Object>> links) throws TransformerException {
+        Map<String, Point> nodePositions = new HashMap<>(); // 存储节点名和坐标
+        Document document = GenericDOMImplementation.getDOMImplementation()
+                .createDocument("http://www.w3.org/2000/svg", "svg", null);
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        Random random = new Random();
+        for (Map<String, Object> node : nodes) {
+            String name = (String) node.get("name");
+            int size = (int) node.get("symbolSize");
+            int x = random.nextInt(500); // 动态生成坐标
+            int y = random.nextInt(300);
+            nodePositions.put(name, new Point(x, y)); // 存储节点位置
+            svgGenerator.setPaint(Color.BLUE);
+            svgGenerator.fillOval(x, y, size, size);
+            svgGenerator.setPaint(Color.BLACK);
+            svgGenerator.drawString(name, x + size, y + size / 2);
+        }
+        svgGenerator.setPaint(Color.RED);
+        svgGenerator.setStroke(new BasicStroke(1.5f));
+        for (Map<String, Object> link : links) {
+            String source = (String) link.get("source");
+            String target = (String) link.get("target");
+            Point start = nodePositions.get(source);
+            Point end = nodePositions.get(target);
+            if (start != null && end != null) {
+                int startX = start.x + ((int) nodes.stream()
+                        .filter(n -> n.get("name").equals(source))
+                        .findFirst().get().get("symbolSize")) / 2;
+                int startY = start.y + ((int) nodes.stream()
+                        .filter(n -> n.get("name").equals(source))
+                        .findFirst().get().get("symbolSize")) / 2;
+                int endX = end.x + ((int) nodes.stream()
+                        .filter(n -> n.get("name").equals(target))
+                        .findFirst().get().get("symbolSize")) / 2;
+                int endY = end.y + ((int) nodes.stream()
+                        .filter(n -> n.get("name").equals(target))
+                        .findFirst().get().get("symbolSize")) / 2;
+                svgGenerator.drawLine(startX, startY, endX, endY);
+            }
+        }
+        Element root = svgGenerator.getRoot();
+        document.getDocumentElement().appendChild(root);
+        TransformerFactory.newInstance()
+                .newTransformer()
+                .transform(new DOMSource(document),
+                        new StreamResult(new File("d://test//dynamic-graph.svg")));
+        System.out.println("SVG 已生成：dynamic-graph.svg");
+    }
+
+
+
+}
---
+++ jquick-pdf/jquick-pdf-svg/src/test/java/RelationTest.java
@@ -0,0 +1,108 @@
+import com.paohaijiao.data.JOption;
+import com.paohaijiao.data.code.JLayout;
+import com.paohaijiao.data.series.JGraph;
+import com.paohaijiao.data.series.force.JLink;
+import com.paohaijiao.echart.generate.JBoxPlotChartRenderer;
+import com.paohaijiao.echart.generate.JRelationChartRenderer;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.*;
+
+public class RelationTest {
+    @Test
+    public void testBarChar1() throws IOException, ParserConfigurationException, TransformerException {
+        JOption option = new JOption();
+        option.title("关系图示例");
+        List<Map<String, Object>> nodes = Arrays.asList(
+                new HashMap<String, Object>() {{
+                    put("name", "CEO");
+                    put("category", "管理");
+                    put("symbolSize", 50);
+                    put("itemStyle", new HashMap<String, String>() {{
+                        put("color", "#c23531");
+                    }});
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "技术部");
+                    put("category", "部门");
+                    put("symbolSize", 40);
+                    put("itemStyle", new HashMap<String, String>() {{
+                        put("color", "#2f4554");
+                    }});
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "产品部");
+                    put("category", "部门");
+                    put("symbolSize", 40);
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "前端组");
+                    put("category", "团队");
+                    put("symbolSize", 30);
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "后端组");
+                    put("category", "团队");
+                    put("symbolSize", 30);
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "UI设计");
+                    put("category", "岗位");
+                    put("symbolSize", 20);
+                }},
+                new HashMap<String, Object>() {{
+                    put("name", "Java开发");
+                    put("category", "岗位");
+                    put("symbolSize", 20);
+                }}
+        );
+        List<Map<String, Object>> links = Arrays.asList(
+                new HashMap<String, Object>() {{
+                    put("source", "CEO");
+                    put("target", "技术部");
+                    put("label", "直属管理");
+                    put("lineStyle", new HashMap<String, Object>() {{
+                        put("width", 3);
+                        put("type", "solid");
+                    }});
+                }},
+                new HashMap<String, Object>() {{
+                    put("source", "技术部");
+                    put("target", "前端组");
+                    put("label", "包含");
+                    put("lineStyle", new HashMap<String, Object>() {{
+                        put("width", 2);
+                        put("type", "dashed");
+                    }});
+                }},
+                new HashMap<String, Object>() {{
+                    put("source", "前端组");
+                    put("target", "UI设计");
+                    put("label", "协作");
+                    put("lineStyle", new HashMap<String, Object>() {{
+                        put("width", 1);
+                        put("type", "dotted");
+                    }});
+                }},
+                new HashMap<String, Object>() {{
+                    put("source", "技术部");
+                    put("target", "产品部");
+                    put("label", "跨部门合作");
+                    put("lineStyle", new HashMap<String, Object>() {{
+                        put("color", "#61a0a8");
+                        put("curveness", 0.3); // 曲线度
+                    }});
+                }}
+        );
+               Document document = GenericDOMImplementation.getDOMImplementation()
+               .createDocument("http://www.w3.org/2000/svg", "svg", null);SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        SVGGraphics2D svgGraphics2D = new SVGGraphics2D(document);
+        JRelationChartRenderer.generateHeatmapSVG(nodes,links);
+    }
+}
